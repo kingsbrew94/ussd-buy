eb92eeaccab5cb742d337e1a80d44c8aeb6d343e
Kingsley Baah Brew <prince@Kingsleys-MacBook-Pro.local>
2023-10-20 21:59:04 +0000

    ussd-buy commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,12 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use OpenAdmin\Admin\Auth\Database\Administrator;
+use OpenAdmin\Admin\Auth\Database\Menu;
+use OpenAdmin\Admin\Auth\Database\Permission;
+use OpenAdmin\Admin\Auth\Database\Role;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +18,77 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Administrator::truncate();
+        Administrator::create([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'name'     => 'Administrator',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // create a role.
+        Role::truncate();
+        Role::create([
+            'name' => 'Administrator',
+            'slug' => 'administrator',
+        ]);
+
+        // add role to user.
+        Administrator::first()->roles()->save(Role::first());
+
+        //create a permission
+        Permission::truncate();
+        Permission::insert([
+            [
+                'name'        => 'All permission',
+                'slug'        => '*',
+                'http_method' => '',
+                'http_path'   => '*',
+            ],
+            [
+                'name'        => 'Dashboard',
+                'slug'        => 'dashboard',
+                'http_method' => 'GET',
+                'http_path'   => '/',
+            ],
+            [
+                'name'        => 'Login',
+                'slug'        => 'auth.login',
+                'http_method' => '',
+                'http_path'   => "/auth/login\r\n/auth/logout",
+            ],
+            [
+                'name'        => 'User setting',
+                'slug'        => 'auth.setting',
+                'http_method' => 'GET,PUT',
+                'http_path'   => '/auth/setting',
+            ],
+            [
+                'name'        => 'Auth management',
+                'slug'        => 'auth.management',
+                'http_method' => '',
+                'http_path'   => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs",
+            ],
+        ]);
+
+        Role::first()->permissions()->save(Permission::first());
+
+        // add default menus.
+        Menu::truncate();
+        Menu::insert([
+            [
+                'parent_id' => 0,
+                'order'     => 1,
+                'title'     => 'Portal',
+                'icon'      => 'icon-server',
+                'uri'       => 'portal',
+            ]
+        ]);
+
+        // add role to menu.
+        // Menu::find(2)->roles()->save(Role::first());
+        // if ($menu) {
+        //     // If the menu item exists, associate a role with it
+        //     $menu->roles()->save(Role::first());
+        // }
     }
 }
